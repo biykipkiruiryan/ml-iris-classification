78755cb0b7aec3ed9ebdd39360f34d0343971d6c
Your Name <your.email@example.com>
2024-10-13 12:43:00 +0300

    creating live web app

---
+++ Irisapp.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Iris Flower Classification')
+
+# Add some text to the app
+st.write("This app classifies Iris flowers into different species based on their features.")
+
